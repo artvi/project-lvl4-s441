ad1e9528423e9a3fb3c7133c9271d3d24e4f96e8
artv <mona@lisa.io>
2019-05-28 17:44:58 +0300

    normalizing logic changed

--- src/normalize.js
+++ src/normalize.js
@@ -1,34 +1,18 @@
+import _ from 'lodash';
+
 export default ({ channels, messages, currentChannelId }) => {
-  const normalizedChannels = channels.reduce((acc, el) => {
-    const { byId, allIds } = acc;
-    const { id } = el;
-    return {
-      byId: { ...byId, [id]: el },
-      allIds: [...allIds, id],
-    };
-  },
-  {
-    byId: {},
-    allIds: [],
-  });
+  const normalizedChannels = {
+    byId: _.keyBy(channels, 'id'),
+    allIds: channels.reduce((acc, el) => [...acc, el.id], []),
+  };
 
-  const init = {};
-  normalizedChannels.allIds.forEach((id) => {
-    init[id] = [];
-  });
-
-  const normalizedMessages = messages.reduce((acc, el) => {
-    const { channelId } = el;
-    const results = acc[channelId];
-    return {
-      ...acc,
-      [channelId]: [...results, el],
-    };
-  }, init);
+  const { allIds } = normalizedChannels;
+  const byChannelIdColl = allIds.reduce((acc, el) => ({ ...acc, [el]: [] }), {});
+  const messagesByChannelId = _.groupBy(messages, 'channelId');
 
   return {
     channels: normalizedChannels,
-    messages: { byChannelId: normalizedMessages },
+    messages: { byChannelId: { ...byChannelIdColl, ...messagesByChannelId } },
     modal: { show: false, data: {} },
     currentChannelId,
   };
